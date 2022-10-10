7926aacf758a45131e9561547e376960fe5c3d53
wangyifeng <2665939276@qq.com>
2022-10-10 21:05:12 +0800

    搭建ssm开发环境提供测试接口

---
+++ src/main/java/com/ilovesshan/common/R.java
@@ -0,0 +1,58 @@
+package com.ilovesshan.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: ilovesshan
+ * @date: 2022/10/5
+ * @description:
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class R {
+
+    public static final int SUCCESS_CODE = 200;
+    public static final int ERROR_CODE_CLIENT = -200;
+    public static final int ERROR_CODE_SERVER = 500;
+
+    public static final String SUCCESS_MESSAGE = "操作成功";
+    public static final String ERROR_MESSAGE = "操作失败";
+
+    private Integer code;
+    private String message;
+    private Object data;
+
+
+    public static R success() {
+        return R.builder().code(R.SUCCESS_CODE).message(R.SUCCESS_MESSAGE).build();
+    }
+
+    public static R success(Object data) {
+        return R.builder().code(R.SUCCESS_CODE).message(R.SUCCESS_MESSAGE).data(data).build();
+    }
+
+    public static R fail() {
+        return R.builder().code(R.ERROR_CODE_CLIENT).message(R.ERROR_MESSAGE).build();
+    }
+
+    public static R fail(Object data) {
+        return R.builder().code(R.ERROR_CODE_CLIENT).message(R.SUCCESS_MESSAGE).data(data).build();
+    }
+
+    public static R error() {
+        return R.builder().code(R.ERROR_CODE_SERVER).message(R.ERROR_MESSAGE).build();
+    }
+
+    public static R error(Object data) {
+        return R.builder().code(R.ERROR_CODE_SERVER).message(R.SUCCESS_MESSAGE).data(data).build();
+    }
+
+}
